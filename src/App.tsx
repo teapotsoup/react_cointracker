843388dbe3e01099a2df3b8d337cc3ec4e60a7ba
seojengwoo0902 <realteapots@vaultmicro.com>
2022-07-20 16:50:36 +0900

    start 3.4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <>
+      <Circle borderColor="blue" bgColor="teal" text="hello" />
+      <Circle bgColor="tomato" />
+    </>
+  );
+}
+
+export default App;
